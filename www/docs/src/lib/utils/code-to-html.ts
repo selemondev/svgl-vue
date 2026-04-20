bc0f7bd377b51940d48439b836ba066015ecd916
selemondev <selemondev19@gmail.com>
2026-04-19 21:16:55 -0600

    feat: add docs and update release workflow

---
+++ www/docs/src/lib/utils/code-to-html.ts
@@ -0,0 +1,36 @@
+import type { BuiltinTheme, BundledLanguage, ShikiTransformer } from 'shiki'
+import { codeToHtml } from 'shiki'
+export interface Themes {
+  [key: string]: BuiltinTheme
+}
+
+export async function convertCodeToHtml(
+  code: string,
+  lang: BundledLanguage,
+  theme: BuiltinTheme | Themes,
+  transformers: ShikiTransformer[] = [],
+  defaultColor?: string,
+  cssVariablePrefix?: string
+) {
+  if (typeof theme === 'string') {
+    return await codeToHtml(code.trim(), {
+      lang,
+      theme,
+      transformers,
+      defaultColor
+    })
+  }
+
+  const normalizedThemes = {
+    light: theme.light,
+    dark: theme.dark ?? theme.light
+  }
+
+  return await codeToHtml(code.trim(), {
+    lang,
+    themes: normalizedThemes,
+    transformers,
+    defaultColor,
+    cssVariablePrefix
+  })
+}
